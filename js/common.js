376e570bb3d313ffd4bbab9c5689de7eb48189e7
slovb789 <dmitridmit@gmail.com>
2024-02-20 02:34:54 +0200

    slider over dots

--- js/common.js
+++ js/common.js
@@ -6,17 +6,26 @@ $(document).ready(function () {
         dots: true,
         appendDots: $('.dots_slider')
     });
-});
 
-$('.faq_accord-item').on('click', function (e) {
-    e.preventDefault()
-    if ($(this).children('.faq_accord-question').hasClass('accord_question-active')) {
-        $(this).children('.faq_accord-question').removeClass('accord_question-active')
-        $(this).children('.faq_accord-text').slideUp()
-    } else {
-        $('.faq_accord-question').removeClass('accord_question-active')
-        $('.faq_accord-text').slideUp()
-        $(this).children('.faq_accord-question').addClass('accord_question-active')
-        $(this).children('.faq_accord_text').slideDown()
-    }
-})
+    $('.faq_accord-item').on('click', function (e) {
+        e.preventDefault()
+        if (!$(this).children('.faq_accord-question').hasClass('accord_question-active')) {
+            $('.faq_accord-question').removeClass('accord_question-active')
+            $('.faq_accord-text').slideUp()
+            $(this).children('.faq_accord-question').addClass('accord_question-active')
+            $(this).children('.faq_accord-text').slideDown()
+
+
+        } else {
+            $(this).children('.faq_accord-question').removeClass('accord_question-active')
+            $(this).children('.faq_accord-text').slideUp()
+            $('.faq_accord-text').slideUp()
+            $('.faq_accord-question').removeClass('accord_question-active')
+        }
+
+    })
+});
+/*$('.faq_accord-text').css('display', 'none')*/
+
+
+
